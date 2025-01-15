a4290de9dc502e1be4866a58b7e795b36d67a9ab
Emmanuel Alonso <emmanuel010205@gmail.com>
2025-01-15 11:20:48 -0600

    operaciones matematicas

---
+++ impresion_variables.py
@@ -0,0 +1,14 @@
+nombre = "emmanuel"
+print (nombre)
+
+
+numero_uno = 15
+numero_dos = 10
+
+
+print (numero_uno+numero_dos)
+
+nombre_completo = "emmanuel"
+apellidos_completos = "Alonso Campos"
+
+print (nombre_completo + " " + apellidos_completos)
---
+++ operaciones.py
@@ -0,0 +1,18 @@
+""" Definicion de dos variaboels en Python para hacer calculos matematicos """
+
+""" Definicion   """
+a = 10
+b = 5
+""" Operaciones matematicas """
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+""" Impresion de resultado """
+
+print ("la suma de a y b es : ", suma)
+print ("la resta de a y b es : ", resta)
+print ("la multiplicacion de a y b: ", multiplicacion)
+print ("la division de a y b es : ", division)
+
